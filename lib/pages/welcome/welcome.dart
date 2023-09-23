f6ac028af71ddf92d0b4d8144f0f3e38f0cae1c7
bentalla108 <tallambaye108@gmail.com>
2023-09-23 20:44:16 +0000

    Login // SingIn page

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,3 +1,5 @@
+import 'package:app_learning/pages/sign_in/sign_in.dart';
+import 'package:app_learning/pages/welcome/notifier/welcome_notifier.dart';
 import 'package:app_learning/pages/welcome/welcome_text.dart';
 import 'package:app_learning/pages/welcome/widget.dart';
 import 'package:dots_indicator/dots_indicator.dart';
@@ -5,13 +7,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-final indexProvider = StateProvider<int>((ref) => 0);
-
 class Welcome extends ConsumerWidget {
   Welcome({super.key});
   final PageController _controller = PageController();
   final List<Map<String, String>> _pageInformation = pageInformation;
-  void onTapFunction(int index) {
+
+  void onTapFunction(int index, BuildContext context) {
     if (index < _pageInformation.length) {
       _controller.animateToPage(
         index + 1,
@@ -19,11 +20,19 @@ class Welcome extends ConsumerWidget {
         curve: Curves.linear,
       );
     }
+
+    if (index == (_pageInformation.length - 1)) {
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) => const SignIn(),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final indexPage = ref.watch(indexProvider);
+    final indexPage = ref.watch(indexDotProvider);
     return Container(
       color: Colors.white,
       child: SafeArea(
@@ -35,14 +44,14 @@ class Welcome extends ConsumerWidget {
               children: [
                 PageView.builder(
                   onPageChanged: (value) {
-                    ref.read(indexProvider.notifier).state = value;
+                    ref.read(indexDotProvider.notifier).changeValue(value);
                   },
                   controller: _controller,
                   itemCount: _pageInformation.length,
                   itemBuilder: (context, index) {
                     return appOnBoardingPage(
                       onTapFunction: () {
-                        onTapFunction(index);
+                        onTapFunction(index, context);
                       },
                       imageurl: _pageInformation[index]['imageurl'].toString(),
                       title: _pageInformation[index]['title'].toString(),
