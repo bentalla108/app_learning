58a7c832f2a05cfc10bd33e29c66d7f8fd1a1aac
bentalla108 <tallambaye108@gmail.com>
2023-09-23 07:10:12 +0000

    Indicator added

--- lib/common/widget/custom_buttom.dart
+++ lib/common/widget/custom_buttom.dart
@@ -1,2 +1,1 @@
-import 'package:app_learning/common/utils/colors.dart';
-import 'package:flutter/material.dart';
+
--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,6 +1,8 @@
 import 'package:app_learning/pages/welcome/welcome_text.dart';
 import 'package:app_learning/pages/welcome/widget.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Welcome extends StatelessWidget {
   Welcome({super.key});
@@ -18,24 +20,45 @@ class Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          PageView.builder(
-            controller: _controller,
-            itemCount: _pageInformation.length,
-            itemBuilder: (context, index) {
-              return appOnBoardingPage(
-                onTapFunction: () {
-                  onTapFunction(index);
-                },
-                imageurl: _pageInformation[index]['imageurl'].toString(),
-                title: _pageInformation[index]['title'].toString(),
-                subTitle: _pageInformation[index]['subTitle'].toString(),
-              );
-            },
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.only(top: 30),
+          child: Scaffold(
+            body: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                PageView.builder(
+                  controller: _controller,
+                  itemCount: _pageInformation.length,
+                  itemBuilder: (context, index) {
+                    return appOnBoardingPage(
+                      onTapFunction: () {
+                        onTapFunction(index);
+                      },
+                      imageurl: _pageInformation[index]['imageurl'].toString(),
+                      title: _pageInformation[index]['title'].toString(),
+                      subTitle: _pageInformation[index]['subTitle'].toString(),
+                    );
+                  },
+                ),
+                Positioned(
+                  bottom: 100,
+                  child: DotsIndicator(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    dotsCount: _pageInformation.length,
+                    decorator: DotsDecorator(
+                        size: const Size.square(9.0),
+                        activeSize: const Size(18.0, 8.0),
+                        activeShape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5))),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
